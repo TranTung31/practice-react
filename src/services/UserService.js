d567c30f1b5bdb083bb4e73b9f69bad129b622fe
Your Name <you@example.com>
2023-05-09 22:50:36 +0700

    07 - Paginate Users

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,8 @@
 import axios from './customize-axios';
 
-const fetchAllUser = () => {
+const fetchAllUser = (page) => {
   // Call API
-  return axios.get("/api/users?page=1");
+  return axios.get(`/api/users?page=${page}`);
 };
 
 export { fetchAllUser };
